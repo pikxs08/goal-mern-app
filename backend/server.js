e022cc6dd97566af20f8ea90d1e637e95cbb65cf
Pieter Slabbert <pieter.slabbert27@gmail.com>
2024-04-29 20:47:11 +0200

    test vercel backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
 
 connectDB();
 
