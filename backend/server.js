f0855b4b8b678189f88a667ef2d7bc527da5584b
Pieter Slabbert <pieter.slabbert27@gmail.com>
2024-04-29 21:13:42 +0200

    updated routes

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,9 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-import http from 'http';
-
-const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -20,31 +18,19 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
-// Create a server object
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  // Write some text to the response
-  res.end('Welcome to my simple Node.js app!');
-});
-
-server.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold);
-});
-
 // Serve front end in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.status(200);
-//     res.send("API is running....");
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200);
+    res.send("API is running....");
+  });
+}
 
 module.exports = app;
